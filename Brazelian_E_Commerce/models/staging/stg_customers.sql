d59fa8ef260e2822150c9b877b6e856d8dc5dd47
Anusara Senanayaka <anusaraudaen@gmail.com>
2026-06-25 00:17:53 +0530

    Deleting main.py Add stg_customer.sql

---
+++ Brazelian_E_Commerce/models/staging/stg_customers.sql
@@ -0,0 +1,31 @@
+{{config(materialized='view')}}
+
+WITH source AS (
+    SELECT * FROM {{ source('bronze_customers', 'raw_customers') }}
+),
+
+cleaned AS (
+    SELECT
+        -- Primary key
+        customer_id, --Used to connect the orders table to the customers table
+
+        customer_unique_id, --Used to uniquely identify each of the customer
+
+        --customer_city — should be in title case (first letter of each word capitalized)
+        INITCAP(TRIM(COALESCE(customer_city,'Unknown'))) AS customer_city,
+        -- customer_state — should be exactly 2 uppercase letters
+        UPPER(TRIM(COALESCE(customer_state, 'XX'))) AS customer_state,
+
+        -- customer_zip_code_prefix — should be exactly 5 digits
+        LPAD(TRIM(customer_zip_code_prefix), 5, '0') AS zip_code_prefix,
+
+
+        -- Audit columns
+        _file_name,
+        _loaded_at
+
+    FROM source
+    WHERE customer_id IS NOT NULL AND customer_unique_id IS NOT NULL
+)
+
+SELECT * FROM cleaned
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from brazelian-e-commerce-dbt!")
-
-
-if __name__ == "__main__":
-    main()
